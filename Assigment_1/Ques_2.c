ffd1156d41f1a331d58a49b0493fb852f082cc9f
STROKE-MODS <hs619689@gmail.com>
2025-11-10 11:29:23 +0530

    Update Ques_2.c

--- Assigment_1/Ques_2.c
+++ Assigment_1/Ques_2.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int hcf(int a,int b){
     int num = (a<b) ? a : b;
-    for(int i= 2 ;i<num;i++){
+    int h=0;
+    for(int i= 2 ;i<=num;i++){
         if(a%i==0 && b%i==0){
-            return i;
+           h=i;
         }
     }
+    return h;
    }
 
 int main(){
@@ -15,9 +17,12 @@ int main(){
     printf("Enter number 2 : ");
     scanf("%d",&number2);
     int c = hcf(number,number2);
+    if(c==0){
+        c=1;
+    }
 
     
-    printf("The HCF of %d andd %d is : %d",number,number2,c);
+    printf("The HCF of %d and %d is : %d",number,number2,c);
     
     return 0;
 }
